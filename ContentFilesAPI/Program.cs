d88856b5e17b306f2f1935d599f9fcd95860ba4c
James Fulford <james.fulford@outlook.com>
2019-03-10 00:21:57 -0500

    WIP(docs): Adding some docs for Program.cs

--- ContentFilesAPI/Program.cs
+++ ContentFilesAPI/Program.cs
@@ -10,13 +10,25 @@ using Microsoft.Extensions.Logging;
 
 namespace ContentFilesAPI
 {
+    /// <summary>
+    /// CLI interface for starting API web service
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Given CLI args, builds and runs a webserver
+        /// </summary>
+        /// <param name="args">CLI args</param>
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Creates a web server with args with specifications from Startup.cs
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
